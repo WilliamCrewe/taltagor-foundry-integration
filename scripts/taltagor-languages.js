8a335892bcf64c3777405e2afb876d053f7bbf9e
WilliamCrewe <willcrewe@live.co.uk>
2025-05-01 16:01:03 -0700

    Taltagor Language Rebuild
    Removing standard Forgotten Realms languages and updating with appropriate Taltagor Language pack.

---
+++ scripts/taltagor-languages.js
@@ -0,0 +1,63 @@
+console.log("Hello World! This code runs immediately when the file is loaded.");
+
+Hooks.on("init", function() {
+	console.log("This code runs once the Foundry VTT software begins its initialization workflow.");
+
+	//Remove unwanted languages from the game.
+	delete CONFIG.DND5E.languages.standard.children.dwarvish;
+    delete CONFIG.DND5E.languages.standard.children.giant;
+    delete CONFIG.DND5E.languages.standard.children.gnomish;
+	delete CONFIG.DND5E.languages.standard.children.goblin;
+	delete CONFIG.DND5E.languages.standard.children.orc;
+	delete CONFIG.DND5E.languages.exotic.children.aarakocra;
+	delete CONFIG.DND5E.languages.exotic.children.deep-speech;
+	delete CONFIG.DND5E.languages.exotic.children.gith;
+	delete CONFIG.DND5E.languages.exotic.children.gnoll;
+	delete CONFIG.DND5E.languages.exotic.children.undercommon;
+	
+});
+
+Hooks.on("ready", function() {
+  console.log("This code runs once core initialization is ready and game data is available.");
+  
+	delete CONFIG.DND5E.languages.exotic.children.primordial
+  
+    //Add new languages into the game.
+    CONFIG.DND5E.languages.standard.children.dragonborn = {
+        label: "Dragonborn"
+	};
+	CONFIG.DND5E.languages.standard.children.human = {
+        label: "Human"
+    };
+	CONFIG.DND5E.languages.standard.children.primordial = {
+		label: "Primordial",
+       children: {
+			aquan: "Aquan",
+			auran: "Auran",
+			ignan: "Ignan",
+			simulan: "Simulan",
+            terran: "Terran"
+        }
+    };
+	CONFIG.DND5E.languages.standard.children.reptilia = {
+        label: "Reptilia"
+    };
+	CONFIG.DND5E.languages.standard.children.tabaxan = {
+        label: "Tabaxan"
+    };
+	CONFIG.DND5E.languages.exotic.children.giant = {
+        label: "Giant"
+    };
+	CONFIG.DND5E.languages.exotic.children.gnomish = {
+        label: "Gnomish"
+    };
+	CONFIG.DND5E.languages.exotic.children.grave_speech = {
+        label: "Grave Speech"
+    };
+	CONFIG.DND5E.languages.exotic.children.mechanus = {
+        label: "Mechanus"
+    };
+	CONFIG.DND5E.languages.exotic.children.orcish = {
+        label: "Orcish"
+    };
+});
