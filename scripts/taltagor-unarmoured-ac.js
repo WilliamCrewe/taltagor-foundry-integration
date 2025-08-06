d39666c4452f2852f8708f18338034a4329a6e44
WilliamCrewe <willcrewe@live.co.uk>
2025-08-06 15:36:42 -0700

    Major Update (See Description)
    Races: Human, Demi-Human, Triton
    Race Overhaul: Updated the metadata of races to all align, and to better read within the filters.
    Spells: All Cantrips, 1st level, 2nd level, and 4th level Taltagor exclusive spells

--- scripts/taltagor-unarmoured-ac.js
+++ scripts/taltagor-unarmoured-ac.js
@@ -10,4 +10,22 @@ Hooks.once("init", () => {
     label: "Latent Barrier",
     formula: "12 + @abilities.dex.mod"
   };
+  
+  // Creating all Human Natural Armour classes
+  CONFIG.DND5E.armorClasses.human11 = {
+    label: "Natural Armour",
+    formula: "11 + @abilities.dex.mod"
+  };
+  CONFIG.DND5E.armorClasses.human12 = {
+    label: "Natural Armour",
+    formula: "12 + @abilities.dex.mod"
+  };
+  CONFIG.DND5E.armorClasses.human13 = {
+    label: "Natural Armour",
+    formula: "13 + @abilities.dex.mod"
+  };
+  CONFIG.DND5E.armorClasses.human17 = {
+    label: "Natural Armour",
+    formula: "17"
+  };
 });
